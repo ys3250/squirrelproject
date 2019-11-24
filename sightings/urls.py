3efc6ab5b2f7cb222d2eba14cbf7e6d8fa8cf9cd
Sarin Indrasukhsri <si2353@columbia.edu>
2019-11-24 17:02:27 -0500

    Added link to add page
    Added link to add page and add the header to the all listings

--- sightings/urls.py
+++ sightings/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.all_sightings, name = 'all_sightings'),
     path('<str:unique_squirrel_id>/', views.sighting_details),
+    path('add/',view.sighting_details),
 ]
 
 app_name = 'sightings'
