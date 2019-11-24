22ce026778a8908d79b99c65f25bb30d1f1a92e8
ys3250 <56941127+ys3250@users.noreply.github.com>
2019-11-24 14:24:55 -0500

    make every sightings clickable

--- sightings/urls.py
+++ sightings/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('sightings/', views.all_sightings, name = 'all_sightings'),
+    path('', views.all_sightings, name = 'all_sightings'),
     path('<str:unique_squirrel_id>/', views.sighting_details),
 ]
 
