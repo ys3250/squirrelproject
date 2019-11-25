b0ea3796225bab910d1d4674852f1863c4dff1f0
Sarin Indrasukhsri <si2353@columbia.edu>
2019-11-25 12:12:06 -0500

    Added Delete function and Loop back to home page

--- sightings/urls.py
+++ sightings/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('', views.all_sightings, name = 'all_sightings'),
     path('add/', views.add_sighting, name = 'add_sighting'),
     path('<str:unique_squirrel_id>/', views.sighting_details),
+    path('<str:unique_squirrel_id>/delete/', views.delete, name='delete'),
     # path('<str:unique_squirrel_id>/', views.DetailView.as_view(), name='detail'),
 ]
 
--- sightings/views.py
+++ sightings/views.py
@@ -19,6 +19,10 @@ def all_sightings(request):
 def sighting_details(request, unique_squirrel_id):
     sightings = get_object_or_404(Squirrel, pk = unique_squirrel_id)
     form = AddForm(request.POST or None, instance=sightings)
+    context = {
+        'sightings': sightings,
+        'form': form,
+    }
     if request.method == 'POST' :
         if form.is_valid():
             model_instance = form.save(commit=False)
@@ -28,7 +32,7 @@ def sighting_details(request, unique_squirrel_id):
         else:
             return HttpResponse('invalid input')
     else:
-        return render(request, 'sightings/detail.html', {'form': form})
+        return render(request, 'sightings/detail.html', context)
     # return HttpResponse(Squirrel.unique_squirrel_id)
 
 def add_sighting(request):
@@ -41,13 +45,23 @@ def add_sighting(request):
             model_instance = form.save(commit=False)
             model_instance.timestamp = timezone.now()
             model_instance.save()
-            return HttpResponse('thanks')
+            return render(request, 'sightings/returnhome.html')
         else:
             return HttpResponse('invalid input')
     # if a GET (or any other method) we'll create a blank form
     else:
         form = AddForm()
         return render(request, 'sightings/addpage.html', {'form': form})
+
+def delete(request, unique_squirrel_id):
+    squirrel = get_object_or_404(Squirrel, pk = unique_squirrel_id)
+    sightings = Squirrel.objects.all()
+    context = {
+        'sightings': sightings,
+        'squirrel': squirrel.unique_squirrel_id
+    }
+    squirrel.delete()
+    return render(request, 'sightings/delete.html', context)
 
 # class DetailView(generic.DetailView):
 #     model = Squirrel
